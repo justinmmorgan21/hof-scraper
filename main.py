a4a2ae446f339725819587b5a2c83b56fd2e17ad
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-08 11:01:44 -0500

    remove all merged code from fantasy_player

--- main.py
+++ main.py
@@ -4,38 +4,28 @@ import webbrowser
 import httpx
 from bs4 import BeautifulSoup
 
-url = 'https://www.fantasypros.com/nfl/players/christian-mccaffrey.php'
+url = 'https://www.houseoffraser.co.uk/men/hoodies-and-sweatshirts'
 headers = {
   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
 }
 
-# def extract_product_data(product):
-#   try:
-#     name = product.find('span', class_='productdescriptionname').text
-#     brand = product.find('span', class_='productdescriptionbrand').text
-#     price = product.find('span', class_='CurrencySizeLarge curprice').text.strip()
-#     print(f'Brand: {brand} Name: {name} Price: {price}')
-#   except Exception as e:
-#     print(e)
+def extract_product_data(product):
+  try:
+    name = product.find('span', class_='productdescriptionname').text
+    brand = product.find('span', class_='productdescriptionbrand').text
+    price = product.find('span', class_='CurrencySizeLarge curprice').text.strip()
+    print(f'Brand: {brand} Name: {name} Price: {price}')
+  except Exception as e:
+    print(e)
 
 def main():
   response = httpx.get(url, headers=headers)
   response_html = response.text
   soup = BeautifulSoup(response_html, 'html.parser')
-  details = soup.find_all('div', class_='clearfix detail')
-  print(details[2].find('span', class_='pull-right').text)
-  image = soup.find('img', class_='side-nav-player-photo-radius-8')
-  print(image.get('src'))
 
-  # URL of the image
-  image_url = image.get('src')
-
-  # Open the image in the default web browser
-  webbrowser.open(image_url)
-
-  # products = soup.find_all('div', class_='s-productthumbbox')
-  # for product in products:
-  #   extract_product_data(product)
+  products = soup.find_all('div', class_='s-productthumbbox')
+  for product in products:
+    extract_product_data(product)
 
 if __name__ == '__main__':
   main()
